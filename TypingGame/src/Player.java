fba45c041502a8b20a7c626b6a93ca67e7027fb5
yasunosuke <42140495+yasunosuke@users.noreply.github.com>
2021-09-18 20:03:12 +0900

    feat: Added some fields and getter, setters @Player

--- TypingGame/src/Player.java
+++ TypingGame/src/Player.java
@@ -1,4 +1,35 @@
 
 public class Player {
+	private final String name;
+	private boolean isCompleted;
+	private int presentStage;
+	
+	public Player(String name, boolean isCompleted, int presentStage) {
+		super();
+		this.name = name;
+		this.isCompleted = isCompleted;
+		this.presentStage = presentStage;
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public boolean isCompleted() {
+		return isCompleted;
+	}
+
+	public void setCompleted(boolean isCompleted) {
+		this.isCompleted = isCompleted;
+	}
+
+	public int getPresentStage() {
+		return presentStage;
+	}
+
+	public void setPresentStage(int presentStage) {
+		this.presentStage = presentStage;
+	}
+	
+	
 }
